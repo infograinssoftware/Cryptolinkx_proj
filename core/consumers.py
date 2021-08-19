6bdba8dc8b032ffa390960997643ae797b70a29f
mohitCodepy <mohit1511199@gmail.com>
2021-08-19 17:50:12 +0530

    adding data to group channels

--- core/consumers.py
+++ core/consumers.py
@@ -71,6 +71,17 @@ class P2PConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_group_name = 'p2p_group'
 
+
+        self.pair_name =  self.scope['url_route']['kwargs']['pair_name']
+
+        if not self.pair_name:
+            self.pair_name = 'BTCUSDT'
+
+        self.single_coin =  await self.getting_single_pair_name()
+        self.json_single_coin = await sync_to_async(serialize)('json', [self.single_coin])
+
+        print(self.json_single_coin)
+
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -83,10 +94,11 @@ class P2PConsumer(AsyncWebsocketConsumer):
         self.fetched_pairs = await self.get_some_pairs()
         self.json_fetched_pairs = await sync_to_async(serialize)('json', self.fetched_pairs)
 
-        await self.send(text_data = json.dumps({'msg' : 'connected', 'fetched_pairs' : self.json_fetched_pairs}))
+        await self.send(text_data = json.dumps({'msg' : 'connected', 'fetched_pairs' : self.json_fetched_pairs, 'single_pair' : self.json_single_coin }))
 
     async def receive(self, text_data):
-        pass
+        print(text_data['msg'])
+        
 
     async def disconnect(self, close_code):
         pass
@@ -100,3 +112,9 @@ class P2PConsumer(AsyncWebsocketConsumer):
         p2p_pair_option = CoinPair.objects.filter(pair_name__endswith = 'USDT')
 
         return p2p_pair_option
+
+#   getting single coin and its price
+    @database_sync_to_async
+    def getting_single_pair_name(self):
+        single_obj = CoinPair.objects.get(pair_name = self.pair_name)
+        return single_obj
