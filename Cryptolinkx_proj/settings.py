629a8e7a4dc1476d8d682dfe42d458047c35bf05
mohitCodepy <mohit1511199@gmail.com>
2021-08-23 18:54:23 +0530

    socket issue of overloading again

--- Cryptolinkx_proj/settings.py
+++ Cryptolinkx_proj/settings.py
@@ -193,8 +193,8 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [("127.0.0.1", 6379)],
-            "capacity": 1500,
-            "expiry": 10,
+            "capacity": 5000,
+            "expiry": 5,
         },
     },
 }
