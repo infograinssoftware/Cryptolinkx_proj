4fa790639711680600899d132d64430c02977853
mohitCodepy <mohit1511199@gmail.com>
2021-08-31 19:13:04 +0530

    modify web3 function

--- core/web3.py
+++ core/web3.py
@@ -1,4 +1,8 @@
 from web3 import Web3
-w3 = Web3(Web3.HTTPProvider('https://rinkeby.infura.io/v3/c22ce0861a6647d8bbd10fd3ebc7970b'))
 
-print(w3.isConnected())
+def web3_status():
+    w3 = Web3(Web3.HTTPProvider('https://rinkeby.infura.io/v3/c22ce0861a6647d8bbd10fd3ebc7970b'))
+    web_status = w3.isConnected()
+    return web3_status
+
+
